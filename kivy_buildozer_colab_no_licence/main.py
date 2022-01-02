db9bb3aa87bc88aae3b259ca439e1ead77954d92
Psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2022-01-02 18:49:53 +0900

    Create main.py

---
+++ kivy_buildozer_colab_no_licence/main.py
@@ -0,0 +1,14 @@
+import kivy
+
+kivy.require('2.0.0')
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class FantasticApp(App):
+    def build(self):
+        return Button(text='test')
+
+
+FantasticApp().run()

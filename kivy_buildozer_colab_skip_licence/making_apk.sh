7ef73e00d688fa2c6d1ee3a13340515efa743eac
Psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2022-01-07 10:25:21 +0900

    Update making_apk.sh

--- kivy_buildozer_colab_skip_licence/making_apk.sh
+++ kivy_buildozer_colab_skip_licence/making_apk.sh
@@ -85,7 +85,7 @@ openjdk-11-jdk
 pip install --upgrade buildozer
 
 # Buildozer Init(making buildozer.spec)
-y 2>/dev/null | buildozer init
+printf 'y' | buildozer init
 
 # Buildozer debug mode(adb logcat)
-y 2>/dev/null | buildozer -v android debug deploy run logcat
+printf 'y' | buildozer -v android debug deploy run logcat
